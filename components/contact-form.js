266fc93f23a09ef81c484e6f974d9f404aacdd50
Mike Engel <mike@mike-engel.com>
2019-02-04 14:40:55 -0700

    feat: Various updates and update floating-label-react

--- components/contact-form.js
+++ components/contact-form.js
@@ -1,120 +1,170 @@
+import React, { useState } from "react";
+import css from "styled-jsx/css";
 import { black } from "../constants/css";
-import FloatingLabel, {
-  floatingStyles,
-  focusStyles,
-  inputStyles,
-  labelStyles
-} from "floating-label-react";
-import React from "react";
+import FloatingLabel from "floating-label-react";
 
-const inputStyle = {
-  floating: Object.assign({}, floatingStyles, {
-    color: black
-  }),
-  focus: Object.assign({}, focusStyles, {
-    borderColor: black
-  }),
-  input: Object.assign({}, inputStyles, {
-    borderBottomWidth: 2,
-    borderColor: black,
-    width: "100%"
-  }),
-  label: Object.assign({}, labelStyles, {
-    marginTop: "0.5em",
-    width: "100%"
-  })
+const { className, styles } = css.resolve`
+.floating-label {
+  box-sizing: border-box;
+  display: block;
+  width: 100%;
+  font-family: -apple-system, BlinkMacSystemFont, Segoe UI, Roboto, Oxygen, Ubuntu,
+    Cantarell, Fira Sans, Droid Sans, Helvetica Neue, sans-serif;
+  padding-top: 5px;
+  position: relative;
+}
+
+.floating-label + .floating-label {
+  margin-top: .5em;
+}
+
+.floating-label :global(input),
+.floating-label :global(textarea) {
+  border: none;
+  border-bottom: 2px solid black;
+  box-sizing: border-box;
+  font-family: -apple-system, BlinkMacSystemFont, Segoe UI, Roboto, Oxygen, Ubuntu,
+    Cantarell, Fira Sans, Droid Sans, Helvetica Neue, sans-serif;
+  font-size: 1rem;
+  padding: 12px 0 8px 0;
+  width: 100%;
+}
+
+.floating-label :global(textarea) {
+  min-height: 150px;
+}
+
+.floating-label :global(input:focus + span),
+.floating-label :global(textarea:focus + span),
+.floating-label.floating :global(span) {
+  font-size: 0.625rem;
+  padding: 0;
+}
+
+.floating-label :global(input:focus:not(:focus-visible)),
+.floating-label :global(textarea:focus:not(:focus-visible)) {
+  outline: none;
+}
+
+.floating-label :global(span) {
+  box-sizing: border-box;
+  font-size: 1rem;
+  left: 0;
+  padding: 14px 0 13px 0;
+  pointer-events: none;
+  position: absolute;
+  top: 0;
+  transition: font-size 200ms, padding 200ms;
+  z-index: 1;
+}
+`;
+
+const updateFormState = cb => evt => {
+  cb(evt.currentTarget.value);
 };
 
-const textareaStyle = Object.assign({}, inputStyle, {
-  input: Object.assign({}, inputStyle.input, {
-    minHeight: "150px"
-  })
-});
+const ContactForm = () => {
+  const [email, setEmail] = useState("");
+  const [subject, setSubject] = useState("");
+  const [body, setBody] = useState("");
 
-const ContactForm = () => (
-  <form
-    className="form contact-form"
-    action="//formspree.io/mike@mike-engel.com"
-    method="POST"
-    role="form"
-  >
-    <fieldset>
-      <input type="hidden" name="_next" aria-hidden="true" value="//mike-engel.com/?thanks=true" />
-      <input type="hidden" name="_gotcha" aria-hidden="true" />
-    </fieldset>
-    <fieldset>
-      <FloatingLabel
-        id="email"
-        name="email"
-        placeholder="Your email"
-        styles={inputStyle}
-        type="email"
-      />
-      <FloatingLabel
-        id="subject"
-        name="_subject"
-        placeholder="A quick summary"
-        styles={inputStyle}
-      />
-      <FloatingLabel
-        id="body"
-        element="textarea"
-        name="body"
-        placeholder="Let loose"
-        styles={textareaStyle}
-      />
-      <button className="btn" type="submit">
-        send
-      </button>
-    </fieldset>
-    <style jsx>
-      {`
-        form {
-          max-width: 40em;
-        }
+  return (
+    <form
+      className="form contact-form"
+      action="//formspree.io/mike@mike-engel.com"
+      method="POST"
+      role="form"
+    >
+      <fieldset>
+        <input
+          type="hidden"
+          name="_next"
+          aria-hidden="true"
+          value="//mike-engel.com/?thanks=true"
+        />
+        <input type="hidden" name="_gotcha" aria-hidden="true" />
+      </fieldset>
+      <fieldset>
+        <FloatingLabel
+          className={className}
+          id="email"
+          name="email"
+          placeholder="Your email"
+          type="email"
+          value={email}
+          onChange={updateFormState(setEmail)}
+        />
+        <FloatingLabel
+          className={className}
+          id="subject"
+          name="_subject"
+          placeholder="A quick summary"
+          value={subject}
+          onChange={updateFormState(setSubject)}
+        />
+        <FloatingLabel
+          className={className}
+          id="body"
+          component="textarea"
+          name="body"
+          placeholder="Let loose"
+          value={body}
+          onChange={updateFormState(setBody)}
+        />
+        <button className="btn" type="submit">
+          send
+        </button>
+      </fieldset>
+      <style jsx>
+        {`
+          form {
+            max-width: 40em;
+          }
 
-        form > span,
-        form > div {
-          position: relative;
-        }
+          form > span,
+          form > div {
+            position: relative;
+          }
 
-        form > span + span,
-        form > div + div {
-          margin-top: 1em;
-        }
+          form > span + span,
+          form > div + div {
+            margin-top: 1em;
+          }
 
-        fieldset {
-          margin: 0;
-          padding: 0;
-          border: none;
-        }
+          fieldset {
+            margin: 0;
+            padding: 0;
+            border: none;
+          }
 
-        fieldset > span,
-        fieldset > div {
-          position: relative;
-        }
+          fieldset > span,
+          fieldset > div {
+            position: relative;
+          }
 
-        fieldset > span + span,
-        fieldset > div + div {
-          margin-top: 1em;
-        }
+          fieldset > span + span,
+          fieldset > div + div {
+            margin-top: 1em;
+          }
 
-        button {
-          background: transparent;
-          border: 2px solid ${black};
-          border-radius: 1px;
-          font: inherit;
-          font-size: 1rem;
-          font-weight: 700;
-          letter-spacing: 0.03em;
-          line-height: 1;
-          margin-top: 1em;
-          padding: 10px 15px;
-        }
-      `}
-    </style>
-  </form>
-);
+          button {
+            background: transparent;
+            border: 2px solid ${black};
+            border-radius: 1px;
+            font: inherit;
+            font-size: 1rem;
+            font-weight: 700;
+            letter-spacing: 0.03em;
+            line-height: 1;
+            margin-top: 1em;
+            padding: 10px 15px;
+          }
+        `}
+      </style>
+      {styles}
+    </form>
+  );
+};
 
 ContactForm.displayName = "ContactForm";
 
