9d076e8aa4bb736067852c5ffc555f056076e8a3
Mike Engel <mike@mike-engel.com>
2019-04-22 22:19:36 -0600

    fix: Overhaul resume to be more descriptive and modern

--- pages/resume.tsx
+++ pages/resume.tsx
@@ -1,19 +1,18 @@
 import React from "react";
-import Link from "next/link";
 import { black, remType } from "../constants/css";
 import { Stylable } from "../types/component.types";
 import styled, { createGlobalStyle } from "styled-components";
 import { MetaTags } from "../components/meta_tags.component";
 
 const GlobalResumeStyles = createGlobalStyle`
-  #__next > header {
-    display: none;
-  }
-
   @media print {
+    #__next > header {
+      display: none;
+    }
+
     html,
     body {
-      font-size: 16px;
+      font-size: 11pt;
     }
 
     .site-constraint {
@@ -28,12 +27,18 @@ const GlobalResumeStyles = createGlobalStyle`
       display: none !important;
     }
 
+    .h3,
+    .h4 {
+      padding: 0 !important;
+    }
+
     .h3 {
       font-size: 1.25rem;
     }
 
     .h4 {
       font-size: 1rem;
+      margin-top: .25em !important;
     }
 
     .text-small + .text-small {
@@ -57,6 +62,7 @@ const GlobalResumeStyles = createGlobalStyle`
     }
 
     .logo {
+      display: initial !important;
       height: 35px !important;
       margin-right: 1em !important;
     }
@@ -79,6 +85,14 @@ const GlobalResumeStyles = createGlobalStyle`
     .resume-education li + li {
       margin-top: 1em !important;
     }
+
+    .bulleted-list {
+      margin: 0.25em 0 0 1em !important;
+
+      li + li {
+        margin-top: 0 !important;
+      }
+    }
   }
 `;
 
@@ -88,25 +102,6 @@ const Resume = ({ className }: Stylable) => (
     <MetaTags title="Resume / Mike Engel" />
     <section>
       <header className="resume-header">
-        <Link href="/">
-          <a className="back-button hidden-link">
-            <svg
-              width="33px"
-              height="25px"
-              viewBox="0 0 33 25"
-              version="1.1"
-              xmlns="http://www.w3.org/2000/svg"
-              xmlnsXlink="http://www.w3.org/1999/xlink"
-            >
-              <g strokeWidth="2" strokeLinecap="square">
-                <path d="M2,12.6066017 L12.6066017,2" />
-                <path d="M2,12.6066017 L12.6066017,23.2132034" />
-                <path d="M3,12.6066017 L32,12.6066017" />
-              </g>
-            </svg>
-            <span>back</span>
-          </a>
-        </Link>
         <svg
           className="logo"
           width="46px"
@@ -121,69 +116,145 @@ const Resume = ({ className }: Stylable) => (
         <ul className="resume-meta">
           <li>Mike Engel</li>
           <li>mike@mike-engel.com</li>
+          <li>United States</li>
+          <li>English, German (A2)</li>
         </ul>
       </header>
-      <h1 className="h3 resume-summary">
-        I&rsquo;m a designer &amp; developer creating pleasant, user driven experiences.
+      <h1 className="h4 resume-summary">
+        Web developer and designer with over 7 years of experience designing and shipping websites
+        and web apps to production. Passionate and focused on creating experiences informed by user
+        research and feedback which work for everyone regardless of ability or device. Successfully
+        leading teams to create new products and update existing ones in collaboration with design,
+        operation, project management, among others.
       </h1>
+      <h2 className="h3" id="skills">
+        Skills
+      </h2>
+      <h3 className="h4">Client side</h3>
+      <p>
+        HTML, CSS, JavaScript, React, Redux, Vue, Next.js, Less, Sass, GraphQL, Webpack, Web
+        Assembly, and Progressive Web Apps
+      </p>
+      <h3 className="h4">Server side</h3>
+      <p>
+        Node.js, Express.js, Next.js, Rust, Swift, GraphQL, REST, SQL (PostgreSQL, MySQL), and NoSQL
+        (RethinkDB, MongoDB)
+      </p>
+      <h3 className="h4">Design</h3>
+      <p>
+        Information Architecture, User experience design, User research, Figma, Sketch, Photoshop,
+        Illustrator, and Indesign
+      </p>
+      <h3 className="h4">Leadership</h3>
+      <p>
+        Technical guidance, Mentoring, One on ones, Inter-team collaboration, Workload management,
+        Career growth, and Work-life balance
+      </p>
+      <h3 className="h4">Miscellaneous</h3>
+      <p>
+        Git, Docker, Testing (TDD), Pair programming, CI/CD, Automation, Agile, XP, Functional
+        programming, Kubernetes, and Authentication &amp; Authorization
+      </p>
       <h2 className="h3" id="experience">
         Experience
       </h2>
       <ul className="resume-experience">
         <li>
-          <h3 className="h4">Self employed</h3>
-          <p className="text-small">Founder / Owner</p>
-          <p className="text-small">January 2019 &ndash; Current</p>
-          <p>
-            Currently working on open source projects and a financial web app to give users a better
-            view into their cash flow across multiple accounts. As the sole developer I've created
-            the concept, user experience, interface design, and front / back end code. It's based
-            completely on web technologies and works on most devices from phones to desktops. More
-            information available upon request.
-          </p>
+          <h3 className="h4">Self employed / Free time</h3>
+          <p className="text-small">Current</p>
+          <ul className="bulleted-list">
+            <li>Creating and maintaining open source projects and managing new issues and PRs</li>
+            <li>
+              Experimenting with new languages and technologies to evaluate the problems they solve
+              and continue learning new concepts and ideas
+            </li>
+            <li>
+              Created and actively developing a financial web app which gives users a better view
+              into their cash flow across multiple accounts. Responsible for all concepts, design,
+              user experience, and development.
+            </li>
+          </ul>
         </li>
         <li>
           <h3 className="h4">Unself</h3>
           <p className="text-small">Senior Developer / Engineering Manager</p>
           <p className="text-small">July 2017 &ndash; December 2018</p>
-          <p>
-            Co-led a small team of full-stack developers to create a mobile-first web app. Helped
-            promote functional programming, simpler testing, type-safety, and several technologies
-            amongst the team. About a year in I began taking on more of an Engineering Manager role
-            to help organize and improve the team.
-          </p>
+          <ul className="bulleted-list">
+            <li>Co-led a small team of full-stack developers creating a mobile-first web app</li>
+            <li>
+              Spearheaded an app-wide visual redesign, collaborating closely with design, product
+              management
+            </li>
+            <li>
+              Successfully gathered consensus and mentored the team on functional programming,
+              better testing practices (simplifying tests, removing redundant tests, etc), type
+              safety, and several technologies to improve quality of life
+            </li>
+            <li>
+              Took on the role of Engineering Manager to better manage individual team members and
+              the entire team's interaction with the rest of the company
+            </li>
+          </ul>
         </li>
         <li>
           <h3 className="h4">Welltok</h3>
           <p className="text-small">Lead software engineer / Team lead</p>
           <p className="text-small">November 2015 &ndash; July 2017</p>
-          <p>
-            Led a team of 8 developers responsible for creating a lightweight, functional node
-            service in addition to maintaining an old Rails app. Led initiatives for better
-            documentation and testing. Co-founded the front end working group in an effort to
-            stabilize, document, and standardize the front end tech stack.
-          </p>
+          <ul className="bulleted-list">
+            <li>
+              Led a team of 8 developers responsible for creating a new lightweight, functional
+              node.js service
+            </li>
+            <li>
+              Mentored junior front end engineers to get them up to speed, more productive, and
+              progress their careers
+            </li>
+            <li>
+              Worked with other senior front end engineers to research and implement better
+              documentation, testing practices, and tooling
+            </li>
+            <li>
+              Co-founded the front end working group in an effort to stabilize, document, and
+              standardize a front end tech stack that was outdated, complicated, and hard to develop
+              for
+            </li>
+          </ul>
         </li>
         <li>
           <h3 className="h4">Datu Health</h3>
           <p className="text-small">UX Engineer</p>
           <p className="text-small">November 2013 &ndash; October 2015</p>
-          <p>
-            Worked with the design and development teams to iteratively create interactive
-            prototypes to assist in product discovery and implementation. Co-created a front end
-            library for sharing CSS and JavaScript with the development team. Created an
-            authenticated prototype site for publicly viewing and presenting interactive prototypes
-            to clients as well as design, development, and leadership.
-          </p>
+          <ul className="bulleted-list">
+            <li>
+              Worked closely with design, development, and client stakeholders to create and
+              iteratively improve interactive prototypes used during product discovery, user
+              research, and implementation
+            </li>
+            <li>
+              Created an authenticated web site for publicly viewing and presenting live prototypes
+              to clients, design, development, and leadership
+            </li>
+            <li>
+              Led the development of a front end library to sharing pixel perfect CSS and modular
+              JavaScript with the development team
+            </li>
+          </ul>
         </li>
         <li>
-          <h3 className="h4">Markit on Demand</h3>
+          <h3 className="h4">IHS Markit (prev. Markit on Demand)</h3>
           <p className="text-small">Interface designer</p>
           <p className="text-small">May 2011 &ndash; November 2013</p>
-          <p>
-            I worked with international financial and energy companies to design social communities,
-            games, trading platforms, and more.
-          </p>
+          <ul className="bulleted-list">
+            <li>
+              Worked with large international financial and energy companies to design social
+              communities, games, and trading platforms
+            </li>
+            <li>Created designs that were localized in at least three different languages</li>
+            <li>
+              Dedicated spare time to lead a group of other designers interested in creating dynamic
+              data visualizations from live data
+            </li>
+          </ul>
         </li>
       </ul>
       <h2 className="h3" id="education">
@@ -196,20 +267,16 @@ const Resume = ({ className }: Stylable) => (
           <p>Bachelor of Fine Arts, Communications Design</p>
         </li>
       </ul>
-      <h2 className="h3" id="skills">
-        Skills
-      </h2>
-      <h3 className="h4">Development</h3>
-      <p>
-        HTML, CSS, JavaScript, Rust, Node.js, Express.js, React, Redux, Vue, Next.js, Less, Sass,
-        Git, Rust, Swift, Docker, GraphQL, SQL (PostgreSQL, MySQL), NoSQL (RethinkDB, MongoDB),
-        Webpack, Functional programming, Kubernetes, Web Assembly, and Progressive Web Apps
-      </p>
-      <h3 className="h4">Design</h3>
-      <p>
-        Information Achitecture, User experience design, the Adobe Creative Suite (Photoshop,
-        Illustrator, Indesign), Sketch 3, Figma
-      </p>
+      <ul className="links">
+        <li>
+          <span className="bold">Portfolio:</span>{" "}
+          <a href="https://mike-engel.com">https://mike-engel.com</a>
+        </li>
+        <li>
+          <span className="bold">Github:</span>{" "}
+          <a href="https://github.com/mike-engel">https://github.com/mike-engel</a>
+        </li>
+      </ul>
     </section>
   </div>
 );
@@ -219,29 +286,14 @@ export default styled(Resume)`
     margin-bottom: 0.25em;
   }
 
-  .back-button {
-    display: block;
-    margin: 1em 0;
-  }
-
-  .back-button > * {
-    display: inline-block;
-    line-height: 1;
-    vertical-align: middle;
-  }
-
-  .back-button svg {
-    margin-right: 0.6em;
-    stroke: ${black};
-  }
-
   .logo,
   .resume-meta {
     display: inline-block;
-    vertical-align: middle;
+    vertical-align: top;
   }
 
   .logo {
+    display: none;
     width: auto;
     height: 50px;
     margin-right: 1.7em;
@@ -257,7 +309,10 @@ export default styled(Resume)`
   }
 
   .resume-summary {
-    margin: 1em 0 0.5em 0;
+    margin: 2em 0 0.5em 0;
+    font-weight: 400;
+    max-width: 40em;
+    line-height: 1.4;
   }
 
   .resume-summary ~ .h3 {
@@ -277,5 +332,23 @@ export default styled(Resume)`
   .resume-experience p,
   .resume-education p {
     margin: 0;
+  }
+
+  .bulleted-list {
+    margin: 1em 0 0 1em;
+    padding-left: initial;
+    list-style-type: disc;
+
+    li + li {
+      margin-top: 0.5em;
+    }
+  }
+
+  .bold {
+    font-weight: 600;
+  }
+
+  .links {
+    margin-top: 1em;
   }
 `;
