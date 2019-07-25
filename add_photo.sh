5359670095d245c1f411d03bb85823c932f701b6
Mike Engel <mike@mike-engel.com>
2019-07-25 15:27:29 -0600

    feat: Add photo add script

---
+++ add_photo.sh
@@ -0,0 +1,13 @@
+#!/usr/bin/env bash
+
+PHOTO="$1"
+
+jq ". |= [$PHOTO] + ." ./constants/photography.json | sponge ./constants/photography.json
+
+git checkout master
+git fetch --all
+git rebase origin/master
+git add ./constants/photography.json
+git commit -S -m "Add new photo"
+git push origin master
+git checkout -
